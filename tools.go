cf1811562f17e50d2db969d9cd52f909aa8550f4
Jon Mars <jon@azls.co>
2015-12-22 21:16:04 +0800

    add parseint

--- tools.go
+++ tools.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"sort"
+	"strconv"
 	"time"
 )
 
@@ -15,4 +16,28 @@ func Now() string {
 func ContainsString(arr []string, keyword string) bool {
 	i := sort.SearchStrings(arr, keyword)
 	return i != len(arr) && arr[i] == keyword
+}
+
+func ParseInt(s string) int {
+	v, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		Errorf("Error parse int %s: %s", s, err.Error())
+	}
+	return int(v)
+}
+
+func ParseInt32(s string) int32 {
+	v, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		Errorf("Error parse int32 %s: %s", s, err.Error())
+	}
+	return int32(v)
+}
+
+func ParseInt64(s string) int64 {
+	v, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		Errorf("Error parse int64 %s: %s", s, err.Error())
+	}
+	return v
 }
