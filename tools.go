4edcc6e6a8f0a769b9631d874370b904a69c2300
Jon Mars <jon@azls.co>
2015-12-30 16:54:17 +0800

    change time format

--- tools.go
+++ tools.go
@@ -7,7 +7,11 @@ import (
 )
 
 func Now() string {
-	return time.Now().Format("2006-01-02T15:04:05")
+	return time.Now().Format("2006-01-02 15:04:05")
+}
+
+func FormatTime(sec int64) string {
+	return time.Unix(sec, 0).Format("2006-01-02 15:04:05")
 }
 
 /**
